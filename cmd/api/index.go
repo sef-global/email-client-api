0831a410f837d783d1e0e7e3daffc1dd6e38714a
mayura_andrew <48531182+mayura-andrew@users.noreply.github.com>
2024-04-27 03:02:53 +0530

    install swagger

--- cmd/api/index.go
+++ cmd/api/index.go
@@ -5,11 +5,16 @@ import (
 	"net/http"
 )
 
-// !Important --> this heathcheckHandler is implemented as a method on application struct
+// swagger:operation GET / root
+// ---
+// summary: The root endpoint of the API.
+// description: This will show all available API endpoints.
+// responses:
+//   '200':
+//     description: Successfully retrieved list of API endpoints.
 func (app *application) rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
     fmt.Fprint(w, `
-	<!DOCTYPE html>
 	<!DOCTYPE html>
 	<html>
 	<head>
@@ -38,11 +43,16 @@ func (app *application) rootHandler(w http.ResponseWriter, r *http.Request) {
 		<div class="container">
 			<h1>Welcome to Our API!</h1>
 			<h2>API Endpoints</h2>
-			<p><strong>GET /v1/healthcheck:</strong> Check the health of the application.</p>
+			<p><strong>GET /api/v1/healthcheck:</strong> Check the health of the application.</p>
+			<p><strong>GET /debug/vars:</strong> Get debug variables.</p>
+			<p><strong>GET /:</strong> Root endpoint.</p>
+			<p><strong>POST /api/v1/send:</strong> Send an email.</p>
+			<p><strong>GET /api/v1/track:</strong> Track an email.</p>
+			<p><strong>GET /api/v1/recipients/:email:</strong> Get information about a specific email recipient.</p>
 			<h2>Contact</h2>
 			<p>Visit our website: <a href="https://mayuraandrew.tech">https://mayuraandrew.tech</a></p>
 		</div>
 	</body>
-	</html>
+	</html>go get -u github.com/go-swagger/go-swagger/cmd/swagger
     `)
 }
